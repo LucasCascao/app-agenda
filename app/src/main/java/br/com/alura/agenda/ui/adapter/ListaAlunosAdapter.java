0a7832c138f922c59758870c4a573cb11d733d13
Lucas Cascão <lucas.cascao@fatec.sp.gov.br>
2020-07-28 10:04:15 -0300

    Inicializado

---
+++ app/src/main/java/br/com/alura/agenda/ui/adapter/ListaAlunosAdapter.java
@@ -0,0 +1,64 @@
+package br.com.alura.agenda.ui.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.alura.agenda.R;
+import br.com.alura.agenda.model.Aluno;
+
+public class ListaAlunosAdapter extends BaseAdapter {
+
+    private final List<Aluno> alunos = new ArrayList<>();
+    private Context context;
+
+    public ListaAlunosAdapter(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return alunos.size();
+    }
+
+    @Override
+    public Object getItem(int posicao) {
+        return alunos.get(posicao);
+    }
+
+    @Override
+    public long getItemId(int posicao) {
+        return alunos.get(posicao).getId();
+    }
+
+    @Override
+    public View getView(int posicao, View view, ViewGroup viewGroup) {
+        View viewCriada = LayoutInflater
+                .from(context)
+                .inflate(R.layout.item_aluno, viewGroup, false);
+        Aluno aluno = alunos.get(posicao);
+        TextView nome = view.findViewById(R.id.item_aluno_nome);
+        nome.setText(aluno.getNome());
+        TextView telefone = view.findViewById(R.id.item_aluno_telefone);
+        telefone.setText(aluno.getTelefone());
+        return viewCriada;
+    }
+
+    public void addAll(List<Aluno> todos) {
+        this.alunos.addAll(todos);
+    }
+
+    public void clear() {
+        this.alunos.clear();
+    }
+
+    public void remove(Aluno aluno) {
+        this.alunos.remove(aluno);
+    }
+}
